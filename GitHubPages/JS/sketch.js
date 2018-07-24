23eed82553998ab8ba639a249d80925925770303
Ben Baker <bensbaker35@gmail.com>
2018-07-24 15:46:45 -0500

    Trying different ways to refrence files(2)

--- GitHubPages/JS/sketch.js
+++ GitHubPages/JS/sketch.js
@@ -14,9 +14,9 @@ const options = {
 
 
 function preload() {
-    hockeyplayers = loadTable('/csvTables/hockeydata.csv', 'header');
-    teamColors = loadTable('csvTables/teamColorData.csv', 'header');
-    cities = loadTable('/csvTables/cityLatLng.csv', 'header');
+    hockeyplayers = loadTable('/GitHubPages/csvTables/hockeydata.csv', 'header');
+    teamColors = loadTable('/GitHubPages/csvTables/teamColorData.csv', 'header');
+    cities = loadTable('/GitHubPages/csvTables/cityLatLng.csv', 'header');
 }
 
 function setup() {
